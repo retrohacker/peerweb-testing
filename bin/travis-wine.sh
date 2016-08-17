0415acebf95ff5bffb8c3462caf03085a2d3725a
William <william.jblankenship@gmail.com>
2016-08-16 20:44:39 -0400

    test

---
+++ bin/travis-wine.sh
@@ -0,0 +1,15 @@
+brew install wine & # send the long living command to background!
+
+minutes=0
+limit=420
+while kill -0 $! >/dev/null 2>&1; do
+  echo -n -e " \b" # never leave evidences!
+
+  if [ $minutes == $limit ]; then
+    break;
+  fi
+
+  minutes=$((minutes+1))
+
+  sleep 60
+done
